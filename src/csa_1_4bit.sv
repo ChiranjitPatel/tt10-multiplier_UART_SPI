5d2972e488b444976965c74cab190b76f0c5e0ac
chiranjitpatel <chiranjitpatel08@gmail.com>
2025-02-05 00:16:29 +0530

    update

--- src/csa_1_4bit.sv
+++ src/csa_1_4bit.sv
@@ -12,14 +12,15 @@ output [4:0]ot;
 wire [6:0]wc;
 wire [2:0]ws;
 
-fa fa_1(ot[0],wc[0],ia[0],ib[0],ic[0]);
-fa fa_2(ws[0],wc[1],ia[1],ib[1],ic[1]);
-ha ha_1(ws[1],wc[2],ia[2],ib[2]);
-ha ha_2(ws[2],wc[3],ia[3],ib[3]);
+fa fa_1(ia[0],ib[0],ic[0],wc[0],ot[0]);
+fa fa_2(ia[1],ib[1],ic[1],wc[1],ws[0]);
+ha ha_1(ia[2],ib[2],wc[2],ws[1]);
+ha ha_2(ia[3],ib[3],wc[3],ws[2]);
 
-ha ha_3(ot[1],wc[4],wc[0],ws[0]);
-fa fa_3(ot[2],wc[5],wc[1],ws[1],wc[4]);
-fa fa_4(ot[3],wc[6],wc[2],ws[2],wc[5]);
+ha ha_3(wc[0],ws[0],ot[1]);
+fa fa_3(wc[1],ws[1],wc[4],wc[5],ot[2]);
+fa fa_4(wc[2],ws[2],wc[5],wc[6],ot[3]);
 //or or_1(ot[4],wc[3],wc[6]);
 assign ot[4] = wc[3]|wc[6];
+
 endmodule
--- src/csa_2_4bit.sv
+++ src/csa_2_4bit.sv
@@ -12,12 +12,12 @@ module csa_2_4bit(
 	wire [4:0]wc;
 	wire [0:0]ws;
 
-	ha half_1(ot[0],wc[0],ia[0],ib[0]);
-	ha half_2(ws[0],wc[1],ia[1],ib[1]);
+	ha half_1(ia[0],ib[0],wc[0],ot[0]);
+	ha half_2(ia[1],ib[1],wc[1],ws[0]);
 
-	ha half_3(ot[1],wc[2],wc[0],ws[0]);
-	fa full_1(ot[2],wc[3],wc[2],wc[1],ib[2]);
-	ha half_4(ot[3],wc[4],wc[3],ib[3]);
+	ha half_3(wc[0],ws[0],wc[2],ot[1]);
+	fa full_1(wc[2],wc[1],ib[2],wc[3],ot[2]);
+	ha half_4(wc[3],ib[3],wc[4],ot[3]);
 
 
 endmodule
