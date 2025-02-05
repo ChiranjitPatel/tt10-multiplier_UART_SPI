fbf7d7bc72f784c478207867a8be5e8e3f5ffc58
chiranjitpatel <chiranjitpatel08@gmail.com>
2025-02-05 21:00:38 +0530

    updated csa2
    Co-Authored-By: Vivek Adi <vivek.adishesha@gmail.com>

--- src/csa_2_4bit.sv
+++ src/csa_2_4bit.sv
@@ -5,19 +5,18 @@ module csa_2_4bit(
 	);
 	
 	input [3:0]ib;
-	input [1:0]ia;
+	input [2:0]ia;
 	output [4:0]ot;
 	
-
 	wire [4:0]wc;
-	wire [0:0]ws;
+	wire [1:0]ws;
 
 	ha half_1(ia[0],ib[0],wc[0],ot[0]);
 	ha half_2(ia[1],ib[1],wc[1],ws[0]);
+	ha half_3(ia[2],ib[2],wc[2],ws[1]);
 
-	ha half_3(wc[0],ws[0],wc[2],ot[1]);
-	fa full_1(wc[2],wc[1],ib[2],wc[3],ot[2]);
-	ha half_4(wc[3],ib[3],wc[4],ot[3]);
-
+	ha half_4(wc[0],ws[0],wc[3],ot[1]);
+	fa full_1(wc[3],wc[1],ws[1],wc[4],ot[2]);
+	fa full_1(wc[4],wc[2],ib[3],ot[4],ot[3]);
 
 endmodule
--- src/i4bit_mul.sv
+++ src/i4bit_mul.sv
@@ -9,7 +9,7 @@ module i4bit_mul(
 	input 	[3:0] b;
 	output 	[7:0] s;
 	
-	wire [19:0]w;
+	wire [20:0]w;
 	
 	i2bit_mul N1 (a[1:0], b[1:0], w[3:0]);
 	i2bit_mul N2 (a[3:2], b[1:0], w[7:4]);
@@ -19,7 +19,7 @@ module i4bit_mul(
 	assign s[1:0] = w[1:0];
 	
 	csa_1_4bit C1 (w[11:8], w[7:4], w[3:2], w[19:16]);
-	csa_2_4bit C2 (w[15:12], w[19:18], s[7:4]);
+	csa_2_4bit C2 (w[15:12], w[20:18], s[7:4]);
 	
 	assign s[3:2] = w[17:16];
 	
