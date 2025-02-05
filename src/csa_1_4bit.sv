8a9d07dabbdd3ccc62d6c501c7759b2975ca8fb2
chiranjitpatel <chiranjitpatel08@gmail.com>
2025-02-05 22:11:55 +0530

    updated test

--- src/csa_1_4bit.sv
+++ src/csa_1_4bit.sv
@@ -21,7 +21,6 @@ ha ha_2(ia[3],ib[3],wc[3],ws[2]);
 ha ha_3(wc[0],ws[0],wc[4],ot[1]);
 fa fa_3(wc[1],ws[1],wc[4],wc[5],ot[2]);
 fa fa_4(wc[2],ws[2],wc[5],wc[6],ot[3]);
-//or or_1(ot[4],wc[3],wc[6]);
-assign ot[4] = wc[3]|wc[6];
+assign ot[4] = wc[3]^wc[6];
 
 endmodule
