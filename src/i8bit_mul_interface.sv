ba947cc0975b6b10bf934036d656d3900b183b18
chiranjitpatel <chiranjitpatel08@gmail.com>
2025-03-09 23:46:15 +0530

    updated i8bit mul and its tb
    Co-Authored-By: Vivek Adi <vivek.adishesha@gmail.com>

--- src/i8bit_mul_interface.sv
+++ src/i8bit_mul_interface.sv
@@ -17,26 +17,31 @@ module i8bit_mul_interface(
 	
 	
 	logic [15:0] mul_op_prod_int;
+	logic mul_start_edge;
 	
-	i8bit_mul
+	i8bit_mul i8bit_mul_uut
 	(
 		.mul_ip_A(mul_ip_BA[7:0]), 
 		.mul_ip_B(mul_ip_BA[15:8]), 
 		.prod_low(mul_op_prod_int[7:0]),
 		.prod_high(mul_op_prod_int[15:8])
 	);
-	
+	always_comb begin
+		mul_op_prod = mul_op_prod_int;
+	end
 	// Mul_ready signal indicates that the multiplier is ready for a new operation
 	always_ff @(posedge clk or negedge reset) begin
 			if (~reset) begin
+				mul_start_edge <= mul_start;
 				mul_ready <= 1'b1;
-				mul_op_prod <= 16'b0;
+				// mul_op_prod <= 16'b0;
 			end 
 			
 			else begin
-				if (mul_start) begin
+				mul_start_edge <= mul_start;
+				if (~mul_start_edge & mul_start) begin
+					// mul_op_prod <= mul_op_prod_int;
 					mul_ready <= 1'b0;
-					mul_op_prod <= mul_op_prod_int;
 				end
 				else
 					mul_ready <= 1'b1;
